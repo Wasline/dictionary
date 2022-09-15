205ff765bed7d789f56487edcebcefd238e43fec
wasline <sf.wasline@gmail.com>
2022-09-15 19:24:06 -0400

    Add first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./choose.jpg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      <h1>DICTIONARY</h1>
+      <p>You just have to ask, I'll execute.</p>
+      </header>
+    </div>
+  );
+}
