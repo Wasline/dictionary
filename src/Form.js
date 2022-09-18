d1feefcb0616f1dd93127dbbc6a1b2bcbebd593b
wasline <sf.wasline@gmail.com>
2022-09-18 18:03:11 -0400

    Added api integration (axios)

--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,29 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Form.css";
 
 export default function Form() {
-let [keyword, setKeyword] = useState(null);
+  let [keyword, setKeyword] = useState(null);
 
-function haandelKeywordChange(event){
+  function handleKeywordChange(event) {
     setKeyword(event.target.value);
-}
+  }
+
+  function handelResponse(response) {
+    console.log(response.data[0]);
+  }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for the definition of the word "${keyword}"`);
+
+//documenttion: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handelResponse);
   }
   return (
     <div className="Form">
       <form onSubmit={search}>
-        <input type="search"
-        onChange={haandelKeywordChange}
-        autoFocus="on" />
+        <input type="search" onChange={handleKeywordChange} autoFocus="on" />
       </form>
     </div>
   );
