e98b464c96a6932e6a4866d519c1903c58abf141
wasline <sf.wasline@gmail.com>
2022-09-18 16:56:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import logo from "./choose.jpg";
+import Form from "./Form";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      <h1>DICTIONARY</h1>
-      <p>You just have to ask, I'll execute.</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>DICTIONARY</h1>
+          <p>You just have to ask, I'll execute.</p>
+        </header>
+        <main>
+          <Form />
+        </main>
+        <footer className="text-center">
+          Coded by Wasline Saint fleur and it's{" "}
+          <a
+            href="https://github.com/Wasline/dictionary"
+            className="text-decoration-none"
+            target="_blank"
+            
+            title="link to github"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
