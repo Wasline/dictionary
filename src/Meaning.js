781c583f4c2b62210fb0384d9b0c44a112b85c94
wasline <sf.wasline@gmail.com>
2022-09-20 19:06:44 -0400

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,23 +2,22 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
+  if (props.meaning) {
     return (
       <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
         {props.meaning.definitions.map(function (definition, index) {
           return (
             <div key={index}>
-              <p>
-                <strong> Definition:</strong> {definition.definition}
-                <br />
-                <strong> Example:</strong>
-                <em>{definition.example}</em>
-                <Synonyms synonyms={definition.synonyms} />
-              </p>
+              <p>{definition.definition}</p>
+              <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
             </div>
           );
         })}
       </div>
     );
- }
+  } else {
+    return null;
+  }
+}
